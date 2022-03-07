02bdb049d803a682d6c70970b01c13ddebbde0fe
Dom Bryan <dombguitar@gmail.com>
2022-03-07 11:45:23 +0000

    Initial Commit

---
+++ HotProspects/HotProspectsApp.swift
@@ -0,0 +1,17 @@
+//
+//  HotProspectsApp.swift
+//  HotProspects
+//
+//  Created by Dom Bryan on 07/03/2022.
+//
+
+import SwiftUI
+
+@main
+struct HotProspectsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
